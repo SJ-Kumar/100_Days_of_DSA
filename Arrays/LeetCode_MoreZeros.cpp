e1e8dea71abc84a5540996dd85fe805aa3e25806
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-02-19 17:38:12 +0530

    MoveZeros

---
+++ Arrays/LeetCode_MoreZeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n){
+            if(nums[i] == 0){
+                i++;
+            }
+            else{
+                if(i != j){
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                i++;
+                j++;
+            }
+        }
+    }
+};
