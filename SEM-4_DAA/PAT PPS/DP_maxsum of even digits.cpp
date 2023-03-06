e10e14970d27d2c001b1264e487b7083862c394c
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-03-06 14:25:13 +0530

    Update DP_maxsum of even digits.cpp

--- SEM-4_DAA/PAT PPS/DP_maxsum of even digits.cpp
+++ SEM-4_DAA/PAT PPS/DP_maxsum of even digits.cpp
@@ -1,1 +1,38 @@
-dynamic programming algorithm to solve the problem of finding the maximum sum of even digits in a number
+/* Dynamic programming algorithm to solve the problem of finding the maximum sum of even digits in a number */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int digitSum(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int dp[n+1];
+    memset(dp, 0, sizeof(dp));
+
+    for (int i = 1; i <= n; i++) {
+        int sum = digitSum(i);
+        if (sum % 2 == 0) dp[i] = sum;
+        else dp[i] = sum + 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            dp[i] = max(dp[i], dp[j] + dp[i-j]);
+        }
+    }
+
+    cout << dp[n] << endl;
+
+    return 0;
+}
