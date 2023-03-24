718405bf5e0470540d1b5a4a1547d2bf62a7e614
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-03-24 12:21:47 +0530

    Update InterviewQs_Maximum Subarray.cpp

--- Arrays/InterviewQs_Maximum Subarray.cpp
+++ Arrays/InterviewQs_Maximum Subarray.cpp
@@ -17,3 +17,33 @@ public:
 
 
 //Divide and Conquer
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        return divideandconquer(nums,0,nums.size()-1);
+    }
+    private:
+        int divideandconquer(vector<int>nums,int left,int right){
+            if(left==right) return nums[left];
+            int mid=left+(right-left)/2;
+            int leftSum=divideandconquer(nums,0,mid);
+            int rightSum=divideandconquer(nums,mid+1,right);
+            int crossSum=crossMidSum(nums,left,right,mid);
+            return max(max(leftSum,rightSum),crossSum);
+        }
+
+        int crossMidSum(vector<int>nums,int left,int right,int mid){
+            int leftMaxSum=INT_MIN;int sum=0;
+            for(int i=mid;i>=left;i--){
+                sum=sum+nums[i];
+                leftMaxSum=max(leftMaxSum,sum);
+            }
+            int rightMaxSum=INT_MIN;sum=0;
+            for(int i=mid+1;i<=right;i++){
+                sum=sum+nums[i];
+                rightMaxSum=max(rightMaxSum,sum);
+            }
+            return leftMaxSum+rightMaxSum;
+        }
+};
