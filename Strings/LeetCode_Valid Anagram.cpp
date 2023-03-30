5aee59368cf020e6f0777ef8a58d7e67c0cd4b86
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-03-30 20:38:58 +0530

    Update LeetCode_Valid Anagram.cpp

--- Strings/LeetCode_Valid Anagram.cpp
+++ Strings/LeetCode_Valid Anagram.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> charCounts;
-        for (char c : s) {
-            charCounts[c]++;
+        // If the two strings have different lengths, they can't be anagrams.
+        if (s.size() != t.size()) {
+            return false;
         }
-        for (char c : t) {
-            charCounts[c]--;
+        
+        // Create a hash map to store the frequency of each character in the first string.
+        unordered_map<char, int> freq;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
         }
-        for (auto p : charCounts) {
-            if (p.second != 0) {
+        
+        // Iterate through the second string and decrement the frequency of each character in the hash map.
+        // If a character's frequency becomes negative, then it means the second string has more occurrences of that character than the first string,
+        // and therefore, the second string can't be an anagram of the first string.
+        for (int i = 0; i < t.size(); i++) {
+            freq[t[i]]--;
+            if (freq[t[i]] < 0) {
                 return false;
             }
         }
+        
+        // If we've reached this point, then the second string is an anagram of the first string.
         return true;
     }
 };
