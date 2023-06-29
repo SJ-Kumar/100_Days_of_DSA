c08395b18d05a544461b77ce514dfe1cc0d63c1f
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-29 12:01:32 +0530

    new

---
+++ Interesting Java Questions/CAT-2/Swap Two Nibbles in a Byte.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int swapn;
+        swapn=((n & 0x0F) << 4 | (n & 0xF0) >> 4);
+        System.out.println(swapn);
+        s.close();
+    }
+}
