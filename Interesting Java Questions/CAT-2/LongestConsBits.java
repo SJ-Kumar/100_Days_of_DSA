190c4127a7c7d0ff06188294150559cd0a86cdf8
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-06-30 07:57:54 +0530

    Update LongestConsBits.java

--- Interesting Java Questions/CAT-2/LongestConsBits.java
+++ Interesting Java Questions/CAT-2/LongestConsBits.java
@@ -1,27 +1,37 @@
 import java.util.*;
+
 class Main {
-    public static int flip(int a){
-        int currLen=0,prevLen=0,maxLen=0;
+    public static int flip(int a, int k) {
+        int currLen=0;int maxLen=0;int prevLen=0;
+        int flips=0;
         while(a!=0){
             if((a&1)==1){
                 currLen++;
             }
-            else if((a&1) == 0)
-            {
-                prevLen = (a&2)== 0 ? 0:currLen;
+            else if((a&1)==0){
+                prevLen=((a&2)==0) ? 0:currLen;
                 currLen=0;
+                if(flips<k){
+                    currLen++;
+                    flips++;
+                }
+                else{
+                    break;
+                }
             }
             maxLen=Math.max(maxLen,prevLen+currLen);
             a>>=1;
         }
-        return maxLen+1;
+        return maxLen +1;
     }
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
-        System.out.println(flip(n));
+        int k=s.nextInt();
+        System.out.println(flip(n,k));
     }
 }
+
 
 //TC - O(n)
 //SC - O(1)
