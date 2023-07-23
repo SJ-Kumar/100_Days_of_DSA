8e2777681ced4b30c2aa592885336d51353b1091
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2023-07-23 06:42:48 +0530

    Create SelectionSort.java

---
+++ Interesting Java Questions/SelectionSort.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element in the remaining unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the minimum element with the first element of the unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("\nSorted array: ");
+        printArray(arr);
+
+        scanner.close();
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
