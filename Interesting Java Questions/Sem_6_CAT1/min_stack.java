6a6bbd5203525ef18d2d60e582c920a4044c0a6b
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2024-02-09 22:54:38 +0530

    Create min_stack.java

---
+++ Interesting Java Questions/Sem_6_CAT1/min_stack.java
@@ -0,0 +1,53 @@
+//TC-O(n)
+//SC-O(n)
+
+import java.util.*;
+
+class Main {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    
+    public Main() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty()) {
+            if(stack.peek().equals(minStack.peek())) {
+                minStack.pop();
+            }
+            stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+    
+    public static void main(String[] args) {
+        Main minStack = new Main();
+        minStack.push(2);
+        minStack.push(3);
+        minStack.pop();
+        minStack.push(4);
+        minStack.push(-1);
+        minStack.top();
+        minStack.push(6);
+        minStack.getMin();
+        minStack.pop();
+        minStack.getMin();
+        System.out.println("Current Min: " + minStack.getMin());
+    }
+}
