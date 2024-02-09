c9898d59112cd18f1aa4db625b72faf47c2ff15e
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2024-02-09 18:27:46 +0530

    Create stock_span.java

---
+++ Interesting Java Questions/Sem_6_CAT1/stock_span.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main{
+    public static class StockSpan{
+        public int[] stocks(int[] prices){
+            int n=prices.length;
+            int[] span = new int[n];
+            span[0]=1;
+            for(int i=1;i<n;i++){
+                int j=i-1;
+                while(j>=0 && prices[i]>=prices[j]){
+                    span[i]++;
+                    j--;
+                }
+            }
+            return span;
+        }
+    }
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        int[] prices = new int[n];
+        for(int i=0;i<n;i++){
+            prices[i]=s.nextInt();
+        }
+        int[] result = new StockSpan().stocks(prices);
+        System.out.println("Span:"+Arrays.toString(result));
+        
+    }
+}
