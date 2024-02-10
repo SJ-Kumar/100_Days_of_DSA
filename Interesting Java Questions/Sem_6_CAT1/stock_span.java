e87b726c4bca834c795337a82d480ca4eee0774f
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2024-02-10 07:22:07 +0530

    Update stock_span.java

--- Interesting Java Questions/Sem_6_CAT1/stock_span.java
+++ Interesting Java Questions/Sem_6_CAT1/stock_span.java
@@ -8,6 +8,7 @@ public class Main{
             int[] span = new int[n];
             span[0]=1;
             for(int i=1;i<n;i++){
+                span[i] = 1;
                 int j=i-1;
                 while(j>=0 && prices[i]>=prices[j]){
                     span[i]++;
