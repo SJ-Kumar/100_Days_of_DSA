60ca993b793a380923f40d042908151e2931a863
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2024-03-29 18:10:15 +0530

    new

---
+++ Interesting Java Questions/Sem_6_CAT2/heapsort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Main{
+    public static void heapify(int arr[],int n,int i){
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+        if(left<n && arr[left]>arr[largest]){
+            largest=left;
+        }
+        if(right<n && arr[right]>arr[largest]){
+            largest=right;
+        }
+        if(largest!=i){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,n,largest);
+        }
+    }
+    public static void HeapSort(int arr[],int n){
+        for(int i=n/2-1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+        for(int i=n-1;i>=0;i--){
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+    }
+    public static void printArray(int arr[],int n){
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        HeapSort(arr,n);
+        printArray(arr,n);
+    }
+}
