80e1df2c5a22e508963b90779b6d1c650a1733a3
S Jayanth Kumar <90379900+SJ-Kumar@users.noreply.github.com>
2024-03-30 10:18:56 +0530

    Update recoverbst.java

--- Interesting Java Questions/Sem_6_CAT2/recoverbst.java
+++ Interesting Java Questions/Sem_6_CAT2/recoverbst.java
@@ -10,53 +10,55 @@ class TreeNode {
     }
 }
 
-public class BSTRecovery {
-    TreeNode firstElement = null;
-    TreeNode secondElement = null;
-    TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
+class Main {
+    static class BSTRecovery {
+        TreeNode firstElement = null;
+        TreeNode secondElement = null;
+        TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
 
-    public TreeNode insert(TreeNode root, int key) {
-        if (root == null)
-            return new TreeNode(key);
-        
-        if (root.val < key) {
-            root.right = insert(root.right, key);
-        } else {
-            root.left = insert(root.left, key);
-        }
-        return root;
-    }
+        public TreeNode insert(TreeNode root, int key) {
+            if (root == null)
+                return new TreeNode(key);
 
-    public void recoverTree(TreeNode root) {
-        // Inorder traversal to find the misplaced elements
-        inorder(root);
-
-        // Swap the values of the two misplaced elements
-        int temp = firstElement.val;
-        firstElement.val = secondElement.val;
-        secondElement.val = temp;
-    }
-
-    private void inorder(TreeNode root) {
-        if (root == null)
-            return;
-
-        // Traverse left subtree
-        inorder(root.left);
-
-        // If this node is smaller than the previous node, it's misplaced
-        if (firstElement == null && root.val < prevElement.val) {
-            firstElement = prevElement;
+            if (root.val < key) {
+                root.right = insert(root.right, key);
+            } else {
+                root.left = insert(root.left, key);
+            }
+            return root;
         }
 
-        if (firstElement != null && root.val < prevElement.val) {
-            secondElement = root;
+        public void recoverTree(TreeNode root) {
+            // Inorder traversal to find the misplaced elements
+            inorder(root);
+
+            // Swap the values of the two misplaced elements
+            int temp = firstElement.val;
+            firstElement.val = secondElement.val;
+            secondElement.val = temp;
         }
 
-        prevElement = root;
+        private void inorder(TreeNode root) {
+            if (root == null)
+                return;
 
-        // Traverse right subtree
-        inorder(root.right);
+            // Traverse left subtree
+            inorder(root.left);
+
+            // If this node is smaller than the previous node, it's misplaced
+            if (firstElement == null && root.val < prevElement.val) {
+                firstElement = prevElement;
+            }
+
+            if (firstElement != null && root.val < prevElement.val) {
+                secondElement = root;
+            }
+
+            prevElement = root;
+
+            // Traverse right subtree
+            inorder(root.right);
+        }
     }
 
     public static void main(String[] args) {
@@ -65,7 +67,7 @@ public class BSTRecovery {
         String[] input = scanner.nextLine().split(" ");
         TreeNode root = null;
         BSTRecovery bstRecovery = new BSTRecovery();
-        
+
         for (String node : input) {
             if (!node.equals("-1")) {
                 int value = Integer.parseInt(node);
